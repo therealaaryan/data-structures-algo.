0aff19e1d14822d941ff94a9cdf55c9cf80073da
Aaryan Mehta <51379599+therealaaryan@users.noreply.github.com>
2023-11-25 19:48:55 +0530

    Time: 103 ms (35.30%), Space: 19.3 MB (25.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = set()
+        inRecursion = set()
+        
         adj = defaultdict(list)
-        visited = [0]*numCourses
-
+        
         for a, b in prerequisites:
             adj[b].append(a)
-
-        def is_cyclic(v):
-            if visited[v] == 1:
-                return True
-            if visited[v] == 2:
+        
+        
+        
+        def isCycle(u):
+            visited.add(u)
+            inRecursion.add(u)
+            
+            for v in adj[u]:
+                if v not in visited and isCycle(v):
+                    return True
+                elif v in inRecursion:
+                    return True
+            
+            inRecursion.remove(u)
+            return False
+        
+        
+        
+        for i in range(numCourses):
+            if i not in visited and isCycle(i):
                 return False
-
-            visited[v] = 1
-            if any(is_cyclic(neigh) for neigh in adj[v]):
-                return True
-            visited[v] = 2
-            return False
-
-        if any(is_cyclic(v) for v in range(numCourses)):
-            return False
-
+        
         return True
